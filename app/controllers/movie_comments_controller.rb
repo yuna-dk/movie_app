23b85a49fd85d404ce14495984540d82828fdbdd
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2024-01-10 09:51:42 +0000

    [Add]8

--- app/controllers/movie_comments_controller.rb
+++ app/controllers/movie_comments_controller.rb
@@ -1,16 +1,17 @@
 class MovieCommentsController < ApplicationController
 
   def create
-    post_movie = PostMovie.find(params[:post_movie_id])
-    comment = current_user.movie_comments.new(movie_comment_params)
-    comment.post_movie_id = post_movie.id
-    comment.save
-    redirect_to post_movie_path(post_movie)
+    @post_movie = PostMovie.find(params[:post_movie_id])
+    @comment = current_user.movie_comments.new(movie_comment_params)
+    @comment.post_movie_id = @post_movie.id
+    @comment.save
+    # redirect_to post_movie_path(post_movie)
   end
 
   def destroy
+    @post_movie = PostMovie.find(params[:post_movie_id])
     MovieComment.find(params[:id]).destroy
-    redirect_to post_movie_path(params[:post_movie_id])
+    # redirect_to post_movie_path(params[:post_movie_id])
   end
 
   private
